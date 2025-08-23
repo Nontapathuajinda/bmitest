805b3db832f8ff3497e93197494a35a8c0fb1f17
Nontapathuajinda <nontapatpoom585@gmail.com>
2025-08-23 17:17:00 +0700

    Update 123.py

--- 123.py
+++ 123.py
@@ -1,15 +1,1 @@
-import streamlit as st
 
-st.set_page_config(page_title='Wellcome to my first Web Application',page_icon='💩')
-kg = st.number_input('นํ้าหนัก (Kg):')
-cm = st.number_input('ส่วนสูง (Cm):')
-if st.button('คำนวณ'):
-    bmi=kg/(cm/100)**2
-    if bmi <= 18.5 :
-        st.info('ผอม')
-    elif bmi < 22.9 :
-        st.success('ปกติ')
-    elif bmi < 24.9 :
-        st.warning('อ้วน1')
-    else :
-        st.error('อ้วนมาก')
